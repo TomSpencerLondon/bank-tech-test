5456794d9c98bce700a075313ed654ab63013d04
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-02 21:39:56 +0000

    Withdraw Account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,4 +9,10 @@ describe Account do
       expect { subject.credit(500) }.to change { subject.balance }.by(500)
     end
   end
+
+  describe '#withdraw' do
+    it 'withdraws and decreases the balance' do
+      expect { subject.withdraw(300) }.to change { subject.balance }.by(-300)
+    end
+  end
 end
