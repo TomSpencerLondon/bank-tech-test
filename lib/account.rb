bcc50124b35e53a09f3628bf40299a48b3e95206
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-04 09:12:03 +0000

    Add transaction to history for credit and withdraw in Account.rb

--- lib/account.rb
+++ lib/account.rb
@@ -4,17 +4,21 @@ class Account
 
   def initialize(balance, transaction, history)
     @balance = balance
-    @tansaction = transaction
+    @transaction = transaction
     @history = history
   end
 
   def credit(amount)
     @balance += amount
+    @history.add_transaction(@transaction.new('credit', amount, @balance))
+    @balance
   end
 
   def withdraw(amount)
     raise 'You do not have the funds' if @balance < amount
     @balance -= amount
+    @history.add_transaction(@transaction.new('debit', amount, @balance))
+    @balance
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,15 +6,25 @@ describe Account do
 
   let(:transaction) { double :transaction, type: nil, balance: nil}
   let(:history) { double :history, transactions: [] }
-  let(:subject) { Account.new(balance, balance, history) }
+  let(:subject) { Account.new(balance, transaction, history) }
 
   it 'is created with a balance of the amount provided upon creation of the account' do
     expect(subject.balance).to eq(1000)
   end
 
+  before(:each) do
+    allow(transaction).to receive(:new)
+    allow(history).to receive(:add_transaction).with(transaction.new)
+
+  end
+
   describe '#credit' do
     it 'adds credit and increases the balance' do
       expect { subject.credit(amount) }.to change { subject.balance }.by(500)
+    end
+    it 'adds the transaction to history' do
+      expect(history).to receive(:add_transaction)
+      subject.credit(amount)
     end
   end
 
@@ -25,6 +35,10 @@ describe Account do
     it 'doesn\'t allow a user to withdraw more than is available' do
       invalid_amount = 2000
       expect{ subject.withdraw(invalid_amount) }.to raise_error('You do not have the funds')
+    end
+    it 'adds the transaction to history' do
+      expect(history).to receive(:add_transaction)
+      subject.withdraw(amount)
     end
   end
 end
